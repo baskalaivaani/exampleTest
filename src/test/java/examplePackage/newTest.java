14a5137dc6bf3d955455c3c5cddfc3b0f7ffc14e
baskalaivaani <baskalaivani@gmail.com>
2022-10-31 10:40:13 +0530

    fix

--- src/test/java/examplePackage/newTest.java
+++ src/test/java/examplePackage/newTest.java
@@ -12,7 +12,7 @@ public class newTest {
   public void openGoogle1() {
 	  System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 	  WebDriver driver = new ChromeDriver();
-	  driver.get("https://www.gmail.com/");
+	  driver.get("https://www.google.com/");
 	  String title = driver.getTitle();
 	  System.out.println("title is " + title);
 	  driver.quit();
